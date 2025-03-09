ec69121a114ce991d2500cf134a080b4e7513b7f
Seungwon Lee <misi1234567@naaver.com>
2025-03-09 15:56:31 +0900

    추상 팩토리 메서드 추가

--- designPattern/src/factoryPattern/Pizza/ConcreteCreator/NYPizzaStore.java
+++ designPattern/src/factoryPattern/Pizza/ConcreteCreator/NYPizzaStore.java
@@ -1,6 +1,10 @@
 package factoryPattern.Pizza.ConcreteCreator;
 
+import factoryPattern.Pizza.ConcreteProduct.NYPizze.FreshClams;
+import factoryPattern.Pizza.ConcreteProduct.NYPizze.MarianSauce;
 import factoryPattern.Pizza.ConcreteProduct.NYPizze.NYPizza;
+import factoryPattern.Pizza.ConcreteProduct.NYPizze.ReggianoCheese;
+import factoryPattern.Pizza.ConcreteProduct.NYPizze.ThickCrustDough;
 import factoryPattern.Pizza.Creator.PizzaStore;
 import factoryPattern.Pizza.Product.Pizza;
 
@@ -8,6 +12,6 @@ public class NYPizzaStore extends PizzaStore {
     
     @Override
     public Pizza createPizza() {
-        return new NYPizza();
+        return new NYPizza(new ThickCrustDough(), new MarianSauce(), new ReggianoCheese(), new FreshClams());
     }
 }
